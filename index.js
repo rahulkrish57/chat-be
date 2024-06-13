2b461e41114156b58bfff62d77165528a9aeffdc
Rahulkrishnan R <rahul.krishnan@talenq.com>
2024-06-13 21:20:41 +0530

    update cors

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://real-time-poll-chat.onrender.com",
+    ],
     methids: ["GET", "POST"],
   },
 });
